a0064dac08b1b795938c6a6ce41c743fee7ea1fc
Jelister203 <93206051+Jelister203@users.noreply.github.com>
2022-09-26 09:49:59 +0300

    just adding existing yatube-project files

---
+++ yatube/posts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+app_name = 'posts'
+urlpatterns = [
+    path('', views.index, name='posts_main'),
+    path('group_list/<slug:slug>/', views.group_posts, name='group_list'),	
+]
