50a75bdbb1416558ecc60f21b9464da28d775acf
Jelister203 <93206051+Jelister203@users.noreply.github.com>
2022-10-11 11:05:42 +0300

    yep

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -3,8 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('auth/', include('users.urls')),
-    # Если какой-то URL не обнаружится в приложении users —
-    # Django пойдёт искать его в django.contrib.auth
     path('auth/', include('django.contrib.auth.urls')),
     path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
